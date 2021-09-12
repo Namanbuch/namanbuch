821f2d00ba36edfc9bd3e1d6e9089e0055f46680
Namanbuch <86761974+Namanbuch@users.noreply.github.com>
2021-09-12 14:41:24 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,31 +6,31 @@
      let buttonInnerHTML = this.innerHTML;
     switch (buttonInnerHTML) {
       case "w":
-        var audio = new Audio("sounds/crash.mp3");
+        var audio = new Audio("crash.mp3");
         audio.play();
         break;
       case "a":
-        let kick = new Audio("sounds/kick-bass.mp3");
+        let kick = new Audio("kick-bass.mp3");
         kick.play();
         break;
       case "s":
-        let snare = new Audio("sounds/snare.mp3");
+        let snare = new Audio("snare.mp3");
         snare.play();
         break;
       case "d":
-        let tom1 = new Audio("sounds/tom-1.mp3");
+        let tom1 = new Audio("tom-1.mp3");
         tom1.play();
         break;
       case "j":
-        let tom2 = new Audio("sounds/tom-2.mp3");
+        let tom2 = new Audio("tom-2.mp3");
         tom2.play();
         break;
       case "k":
-        let tom3 = new Audio("sounds/tom-3.mp3");
+        let tom3 = new Audio("tom-3.mp3");
         tom3.play();
         break;
       case "l":
-        let tom4 = new Audio("sounds/tom-4.mp3");
+        let tom4 = new Audio("tom-4.mp3");
         tom4.play();
         break;
       default:
