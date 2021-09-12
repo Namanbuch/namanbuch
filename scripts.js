962f745c44ed3fae067175e495679c89baa68118
Namanbuch <86761974+Namanbuch@users.noreply.github.com>
2021-09-12 14:37:44 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,40 @@
+ let numberofDrumButtons = document.querySelectorAll(".drum").length;
+
+
+ for (let i = 0; i < numberofDrumButtons; i++) { document.querySelectorAll(".drum")[i].addEventListener("click", () => {
+      
+     let buttonInnerHTML = this.innerHTML;
+    switch (buttonInnerHTML) {
+      case "w":
+        var audio = new Audio("sounds/crash.mp3");
+        audio.play();
+        break;
+      case "a":
+        let kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+        break;
+      case "s":
+        let snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+      case "d":
+        let tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+      case "j":
+        let tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+      case "k":
+        let tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+      case "l":
+        let tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+      default:
+        alert("hello");
+    }
+  });
+}
